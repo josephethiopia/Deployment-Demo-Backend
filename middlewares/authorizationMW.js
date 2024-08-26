c59a59b3f0033a6369f48d089527016005e73ecc
abel <abelgetachew143@gmail.com>
2024-08-26 13:06:25 +0300

    changed how the token is recieved and used

--- middlewares/authorizationMW.js
+++ middlewares/authorizationMW.js
@@ -4,16 +4,11 @@ dotenv.config();
 
 function authorize() {
     return (req, res, next) => {
-        const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1];
-
+        const token = req.headers['authorization'];
         if(!token) return res.status(401).json({error: "Empty token"});
 
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-            if(decoded.role != 'Admin') return res.status(403).json({error: 'Unauthorized access'});
-
             req.user = decoded;
             next();
         }
